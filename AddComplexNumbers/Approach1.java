fc46c6d748ab6c8dca2d7494b33a7fa3b7e8922e
skuvekar <71187594+skuvekar@users.noreply.github.com>
2024-06-24 13:13:57 +0530

    Approach1.java

--- AddComplexNumbers/Approach1.java
+++ AddComplexNumbers/Approach1.java
@@ -2,17 +2,20 @@
  *
  * @author shruti.kuvekar
  */
+//self created solution
 public class Approach1 {
 
-    public static void addComplexNumbers(String a, String b) {
-        int ArealNumber  = Integer.parseInt(a.split("\\+")[0].trim());
-        int AimaginaryNumber = Integer.parseInt(a.split("\\+")[1].split("i")[0]);
-        int BrealNumber  = Integer.parseInt(b.split("\\+")[0].trim());
-        int BimaginaryNumber = Integer.parseInt(b.split("\\+")[1].split("i")[0]);
-        System.out.println(ArealNumber);
-        System.out.println(AimaginaryNumber);
-        System.out.println(BrealNumber);
-        System.out.println(BimaginaryNumber);
+   public static String addComplexNumbers(String a, String b) {
+        float ArealNumber  = Float.parseFloat(a.split("\\+")[0].trim());
+        float AimaginaryNumber = Float.parseFloat(a.split("\\+")[1].split("i")[0]);
+        float BrealNumber  = Float.parseFloat(b.split("\\+")[0].trim());
+        float BimaginaryNumber = Float.parseFloat(b.split("\\+")[1].split("i")[0]);
+
+        float resultReal = ArealNumber+BrealNumber;
+        float resultImaginary = AimaginaryNumber+BimaginaryNumber;
+        
+        return resultReal+" + "+resultImaginary+"i";
+        
     }
 
     public static void main(String[] args) {
@@ -21,10 +24,25 @@ public class Approach1 {
         String Bcomplexnumber = "";
         System.out.print("\nEnter complex number a : ");
         Acomplexnumber = sc.nextLine();
-        System.out.println("\nEnter complex number b : ");
+        System.out.print("\nEnter complex number b : ");
         Bcomplexnumber = sc.nextLine();
         
-        addComplexNumbers(Acomplexnumber, Bcomplexnumber);
+        System.out.println();
+        System.out.println("The result is as follows: ");
+        System.out.println("("+Acomplexnumber+") + ("+Bcomplexnumber+") = "+addComplexNumbers(Acomplexnumber, Bcomplexnumber));
 
     }
 }
+
+
+output:
+run:
+
+Enter complex number a : 2+3i
+
+Enter complex number b : 4+4i
+
+The result is as follows: 
+(2+3i) + (4+4i) = 6.0 + 7.0i
+BUILD SUCCESSFUL (total time: 16 seconds)
+
